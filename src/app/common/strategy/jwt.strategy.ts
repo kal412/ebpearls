2083ab6ff1070c63d62435d4cb6344ed96615142
kalyan.devkota <kalyan.devkota@ekbana.info>
2024-11-14 19:14:11 +0545

    core: added strategy

---
+++ src/app/common/strategy/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from '../../user/user.service';
+import { User } from '../../user/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly userService: UserService,
+    private readonly _configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: _configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const { username } = payload;
+    const user: User = await this.userService.findOneByUsername(username);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
